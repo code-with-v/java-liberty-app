7a70a96b1533576431af864c34d17891f7aa1d66
marifse <50728232+marifse@users.noreply.github.com>
2022-12-31 20:04:34 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,18 +21,6 @@ provider "ibm" {
   ibmcloud_api_key   = var.ibmcloud_api_key
 }
 
-#resource "ibm_container_cluster" "cluster" {
-#  name              = var.cluster_name
-#  datacenter        = var.datacenter
-#  default_pool_size = var.default_pool_size
-#  machine_type      = var.machine_type
-#  hardware          = var.hardware
-#  kube_version      = var.kube_version
-#  #public_vlan_id    = var.public_vlan_num
-#  #private_vlan_id   = var.private_vlan_num
-#  resource_group_id = data.ibm_resource_group.group.id
-#}
-
 resource "ibm_resource_instance" "cd_instance_1" {
   name              = "cloudant-terraform-service-node-${random_string.random.result}"
   service           = "continuous-delivery"
